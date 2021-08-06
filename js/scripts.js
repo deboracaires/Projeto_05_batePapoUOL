533e644caf9221a74e92feb440a3ea756ec3fba1
deboracaires <debora.caires.mor@gmail.com>
2021-08-06 18:41:31 -0300

    validar acesso do usuario, caso seja invalido continua pedindo ate que um usuario valido seja inserido

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-
-let nomeUsuario = prompt("Digite o seu nome");
 let mensagens = [];
-let novasMensagens = [];
+
+// entrar na sala
+let nomeUsuario = '';
+
+entrarSala();
+function entrarSala(){
+
+    nomeUsuario = prompt("Digite o seu nome");
+    const dados = { name: nomeUsuario};
+    const requisicao = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants', dados);
+    requisicao.catch(tratarError);
+}
+
+function tratarError(erro){
+    alert("Nome inválido ou já existente. Por favor, tente novamente.");
+    entrarSala();
+}
+
+// 
 
 //altera o tamanho da barra de digitar texto quando o usuário clicar
 function digitarMensagem(elemento){
@@ -22,7 +38,7 @@ function processarMensagens(resposta) {
 }
 
 
-
+//coloca as mensagens na tela
 function renderizarMensagens(){
     const chat = document.querySelector("ul");
     chat.innerHTML = '';
@@ -32,16 +48,19 @@ function renderizarMensagens(){
             <h1 class="mensagem entrada-saida">
             <span class="horario">${mensagens[i].time} </span> <strong>${mensagens[i].from}</strong> ${mensagens[i].text} 
             </h1>`;
-        }
-        if(mensagens[i].type === "message"){
+        }else if(mensagens[i].type === "message"){
             chat.innerHTML += `
             <h1 class="mensagem padrao">
             <span class="horario">${mensagens[i].time} </span> <strong class="remetente">${mensagens[i].from}</strong> <span class="para">para</span>
             <strong class="destinatario">${mensagens[i].to} </strong> <span class="conteudo">${mensagens[i].text}</span>
             </h1>`;
+        }else if(mensagens[i].type === "private_message" && mensagens[i].to === nomeUsuario){
+            //colocar isso aqui, faz parte do bonus
         }
     } 
     chat.lastChild.scrollIntoView();
 }
+
+
 
 
