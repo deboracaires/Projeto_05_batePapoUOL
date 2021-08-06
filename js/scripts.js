6b0b2c4bff444fb807587219f51ed04090c71e50
deboracaires <debora.caires.mor@gmail.com>
2021-08-06 17:07:02 -0300

    as mensagens são buscadas com o intervalo de 3 segundos e são atualizadas na tela

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,31 @@
 
 let nomeUsuario = prompt("Digite o seu nome");
 let mensagens = [];
+let novasMensagens = [];
 
+//altera o tamanho da barra de digitar texto quando o usuário clicar
 function digitarMensagem(elemento){
     elemento.style.width = '80vw';
 }
 
+//pega as mensagens no servidor
+setInterval(buscarMensagens, 3000);
+
+function buscarMensagens(){
 const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages');
 promessa.then(processarMensagens);
+}
 
 function processarMensagens(resposta) {
 	mensagens = resposta.data;
     renderizarMensagens();
 }
-const chat = document.querySelector("ul");
+
+
 
 function renderizarMensagens(){
-    console.log(mensagens);
-    console.log(chat.innerHTML);
+    const chat = document.querySelector("ul");
+    chat.innerHTML = '';
     for(let i = 0; i < mensagens.length; i++){
         if(mensagens[i].type === "status"){
             chat.innerHTML += `
@@ -32,8 +40,8 @@ function renderizarMensagens(){
             <strong class="destinatario">${mensagens[i].to} </strong> <span class="conteudo">${mensagens[i].text}</span>
             </h1>`;
         }
-    }
-    
+    } 
+    chat.lastChild.scrollIntoView();
 }
 
 
