3e2383e35f80c8bad1c5e8e0fbb6c1ab176071cf
deboracaires <debora.caires.mor@gmail.com>
2021-08-06 08:26:48 -0300

    o campo de digitar mensagem aumenta o tamanho ao clique do usuário

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+let nomeUsuario = prompt("Digite o seu nome");
+let mensagem = '';
+
+const verificarMensagem = setInterval(digitandoMensagem, 1000);
+
+// function digitandoMensagem(){
+//     mensagem = document.getElementById("mensagem").value;
+// }
+
+function digitarMensagem(elemento){
+    elemento.style.width = '80vw';
+}
+
+
+
