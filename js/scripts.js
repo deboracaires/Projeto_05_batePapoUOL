5d9c96f9d165ef3981755c47f677377e7b0d3bbc
deboracaires <debora.caires.mor@gmail.com>
2021-08-06 11:48:38 -0300

    mensagens são renderizadas mas sem nenhum limite ou contador. no html troquei o div.chat por ul.chat

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
 
 let nomeUsuario = prompt("Digite o seu nome");
-let mensagem = '';
-
-const verificarMensagem = setInterval(digitandoMensagem, 1000);
-
-// function digitandoMensagem(){
-//     mensagem = document.getElementById("mensagem").value;
-// }
+let mensagens = [];
 
 function digitarMensagem(elemento){
     elemento.style.width = '80vw';
 }
 
+const promessa = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/messages');
+promessa.then(processarMensagens);
+
+function processarMensagens(resposta) {
+	mensagens = resposta.data;
+    renderizarMensagens();
+}
+const chat = document.querySelector("ul");
+
+function renderizarMensagens(){
+    console.log(mensagens);
+    console.log(chat.innerHTML);
+    for(let i = 0; i < mensagens.length; i++){
+        if(mensagens[i].type === "status"){
+            chat.innerHTML += `
+            <h1 class="mensagem entrada-saida">
+            <span class="horario">${mensagens[i].time} </span> <strong>${mensagens[i].from}</strong> ${mensagens[i].text} 
+            </h1>`;
+        }
+        if(mensagens[i].type === "message"){
+            chat.innerHTML += `
+            <h1 class="mensagem padrao">
+            <span class="horario">${mensagens[i].time} </span> <strong class="remetente">${mensagens[i].from}</strong> <span class="para">para</span>
+            <strong class="destinatario">${mensagens[i].to} </strong> <span class="conteudo">${mensagens[i].text}</span>
+            </h1>`;
+        }
+    }
+    
+}
 
 
